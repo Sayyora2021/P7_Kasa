35fe44c7bc526d07c5d59fcd62cb9ea457b59fd9
Sayyora2021 <sayyorapardaeva@gmail.com>
2022-12-02 15:33:27 +0100

    404-prb-of-txt-center

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="404">
-      <h2>404</h2>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/">Retournez sur la page d'accueil</Link>
-    </div>
+    <main className="error">
+      <h1>404</h1>
+      <span className="errTxt">
+        Oups! La page que vous demandez n'existe pas.
+      </span>
+      <Link className="link" to="/">
+        Retournez sur la page d'accueil
+      </Link>
+    </main>
   );
 };
 
