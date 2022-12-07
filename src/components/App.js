d48b4b85109eac912d4b4024fc12c7f8926f0ddc
Sayyora2021 <sayyorapardaeva@gmail.com>
2022-12-07 15:11:26 +0100

    about-tablette-font

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home";
 import Lodge from "../pages/Lodge";
-import Navigation from "./Home/Navigation";
+import Header from "./Home/Header";
 import Footer from "./Footer";
 import NotFound from "../pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navigation />
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
